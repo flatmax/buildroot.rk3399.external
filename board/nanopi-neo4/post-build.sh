6c6a34991f428a01841cdd02587af5d7dbf6c6b8
Matt Flax <flatmax@flatmax.org>
2018-12-06 09:53:34 +1100

    Now generate an sdcard.img file.

--- board/nanopi-neo4/post-build.sh
+++ board/nanopi-neo4/post-build.sh
@@ -2,18 +2,20 @@
 
 BOARD_DIR="$(dirname $0)"
 
-# copy uboot files required over :
+# copy over uboot files
 UBOOT_BUILD_DIR=`find ${BUILD_DIR} -type d -name 'uboot-*'`
 cp $UBOOT_BUILD_DIR/uboot.img ${BINARIES_DIR}/
 cp $UBOOT_BUILD_DIR/trust.img ${BINARIES_DIR}/
 cp $UBOOT_BUILD_DIR/rk3399_loader_v1.12.109.bin ${BINARIES_DIR}/sd-fuse-rk3399/buildroot/MiniLoaderAll.bin
 
+# copy over the kernel images
 LINUX_BUILD_DIR=`find ${BUILD_DIR} -type d -name 'linux-nano*'`
 cp $LINUX_BUILD_DIR/kernel.img ${BINARIES_DIR}/
 cp $LINUX_BUILD_DIR/resource.img ${BINARIES_DIR}/
 #cp $LINUX_BUILD_DIR/boot.img ${BINARIES_DIR}/
 cp ${BOARD_DIR}/boot.img ${BINARIES_DIR}/
 
+# setup the image creation software
 cp ${BOARD_DIR}/parameter.txt ${BINARIES_DIR}/sd-fuse-rk3399/buildroot
 ln -sf ${BINARIES_DIR}/kernel.img ${BINARIES_DIR}/sd-fuse-rk3399/buildroot
 ln -sf ${BINARIES_DIR}/resource.img ${BINARIES_DIR}/sd-fuse-rk3399/buildroot
@@ -21,6 +23,10 @@ ln -sf ${BINARIES_DIR}/boot.img ${BINARIES_DIR}/sd-fuse-rk3399/buildroot
 ln -sf ${BINARIES_DIR}/uboot.img ${BINARIES_DIR}/sd-fuse-rk3399/buildroot
 ln -sf ${BINARIES_DIR}/trust.img ${BINARIES_DIR}/sd-fuse-rk3399/buildroot
 ln -sf ${BINARIES_DIR}/rootfs.ext2 ${BINARIES_DIR}/sd-fuse-rk3399/buildroot/rootfs.img
+
+# create the sdcard image
+${BINARIES_DIR}/sd-fuse-rk3399/mkimage.sh buildroot
+ln -sf ${BINARIES_DIR}/rk3399*.img ${BINARIES_DIR}/sdcard.img
 
 # echo "Creating the params.bin file"
 # PARAMS_NAME=${BINARIES_DIR}/params.bin
@@ -34,14 +40,15 @@ ln -sf ${BINARIES_DIR}/rootfs.ext2 ${BINARIES_DIR}/sd-fuse-rk3399/buildroot/root
 #wget https://raw.githubusercontent.com/rockchip-linux/device-custom/master/rk3399/parameter-buildroot.txt -O ${BINARIES_DIR}/parameter.txt
 
 # BOARD_DIR="$(dirname $BOARD_DIR)/board"
-GENIMAGE_CFG="${BOARD_DIR}/genimage.cfg"
-GENIMAGE_TMP="${BUILD_DIR}/genimage.tmp"
 
 #echo BOARD_DIR $BOARD_DIR
 #cp ${BOARD_DIR}/idbloader.img ${BINARIES_DIR}/
 #cp ${BOARD_DIR}/param4sd.txt ${BINARIES_DIR}/
 return
+
 # genimage doesn't work, use sd-fuse-rk3399 in the images directory
+GENIMAGE_CFG="${BOARD_DIR}/genimage.cfg"
+GENIMAGE_TMP="${BUILD_DIR}/genimage.tmp"
 rm -rf "${GENIMAGE_TMP}"
 
 genimage                           \
