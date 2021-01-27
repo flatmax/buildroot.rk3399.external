555594ffc3ac61d750f173fa9bbe7701f29cae8a
Matt Flax <flatmax@flatmax.org>
2021-01-28 08:08:46 +1100

    added the example-module-overlay

---
+++ package/example-module-overlay/example-module-overlay.mk
@@ -0,0 +1,24 @@
+################################################################################
+#
+# example-module-overlay
+#
+################################################################################
+
+EXAMPLE_MODULE_OVERLAY_VERSION = 0.0.1
+EXAMPLE_MODULE_OVERLAY_SITE = $(BR2_EXTERNAL)/package/example-module-overlay
+EXAMPLE_MODULE_OVERLAY_SITE_METHOD = local
+EXAMPLE_MODULE_OVERLAY_DEPENDENCIES =
+
+EXAMPLE_MODULE_OVERLAY_MODULE_SUBDIRS = module-overlay-rk33xx
+EXAMPLE_MODULE_OVERLAY_MODULE_MAKE_OPTS = \
+	INSTALL_MOD_DIR=kernel/sound/soc/rockchip \
+	CONFIG_SND_SOC_MODULE_OVERLAY_RK33XX=m
+
+define EXAMPLE_MODULE_OVERLAY_INSTALL_MOD
+	mkdir -p ${BINARIES_DIR}/rockchip/overlays
+	$(INSTALL) -D -m 644 $(@D)/$(EXAMPLE_MODULE_OVERLAY_MODULE_SUBDIRS)/module-overlay-rk33xx.dtbo ${BINARIES_DIR}/rockchip/overlays/
+endef
+EXAMPLE_MODULE_OVERLAY_POST_INSTALL_TARGET_HOOKS += EXAMPLE_MODULE_OVERLAY_INSTALL_MOD
+
+$(eval $(kernel-module))
+$(eval $(generic-package))
