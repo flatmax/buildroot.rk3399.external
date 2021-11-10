f6ea9ad4db957d1351849aa4cb0531f6cae2bca4
Matt Flax <flatmax@flatmax.org>
2021-11-10 15:16:57 +1100

    Binaries necessary to get volumio to boot to a shell on console.

---
+++ board/nanopi-neo4/bootVolumio.cmd
@@ -0,0 +1,40 @@
+setenv load_addr "0x6000000"
+echo importing vars.txt
+load mmc ${devnum} ${load_addr} vars.txt
+env import -t ${load_addr} ${filesize}
+
+echo setting bootargs then loading the fdt and kernel
+setenv bootargs "root=/dev/mmcblk1p2 earlyprintk console=ttyS2,115200n8 rw rootwait"
+fatload mmc ${devnum}:${distro_bootpart} ${fdt_addr_r} ${fdt_name}
+fatload mmc ${devnum}:${distro_bootpart} ${kernel_addr_r} ${kernel_name}
+
+echo setting up fdt for overlays
+# append overlays as required
+setenv overlay_error "false"
+fdt addr ${fdt_addr_r}
+fdt resize 65536
+for overlay_file in ${overlays}; do
+  echo "loading overlay ${overlay_file}"
+	if fatload mmc ${devnum}:${distro_bootpart} ${load_addr} rockchip/overlays/${overlay_file}.dtbo; then
+		echo "Applying kernel provided DT overlay ${overlay_file}.dtbo"
+		fdt apply ${load_addr} || setenv overlay_error "true"
+	fi
+done
+if test "${overlay_error}" = "true"; then
+	echo "Error applying DT overlays, restoring original DT"
+  fatload mmc ${devnum}:${distro_bootpart} ${fdt_addr_r} ${fdtfile}
+fi
+
+echo importing varsVolumio.txt
+load mmc ${devnum} ${load_addr} varsVolumio.txt
+env import -t ${load_addr} ${filesize}
+
+echo loading the ramdisk
+setenv volumioenv "/dev/mmcblk1p2"
+setenv bootargs "consoleblank=0 scandelay root=${volumioenv} rw console=${console} rootfstype=ext4 loglevel=${verbosity} rootwait ${extraargs} "
+#setenv bootargs "root=${volumioenv} earlyprintk console=ttyS2,115200n8 rootfstype=ext4 loglevel=${verbosity} rw rootwait ${extraargs} "
+#fatload mmc ${devnum}:${distro_bootpart} ${ramdisk_addr_r} uInitrd
+fatload mmc ${devnum}:${distro_bootpart} 0x0a200000 uInitrd
+echo booting the kernel
+#booti ${kernel_addr_r} ${ramdisk_addr_r} ${fdt_addr_r}
+booti ${kernel_addr_r} 0x0a200000 ${fdt_addr_r}
--- board/nanopi-neo4/mkRk3399Volumio.bin.sh
+++ board/nanopi-neo4/mkRk3399Volumio.bin.sh
@@ -3,6 +3,7 @@ BINARIES_DIR=$1 # first argument is the base binaries directory
 PLAT=$2 # second argument is the platform name
 LINUX_DIR=$3 # third argument is the linux src directory
 MODEL=$4 # the third argument is the model
+MKIMAGE=$5
 VOL_PLAT_DIR=$BINARIES_DIR/volumio/platform-$PLAT
 VOL_DIR=$VOL_PLAT_DIR/$MODEL
 boardDir=`dirname $0 | while read a; do cd $a && pwd && break; done`
@@ -17,20 +18,25 @@ echo
 rm -rf $VOL_DIR
 rm -f $BINARIES_DIR/$MODEL.tar.xz
 
-# boot
+# u-boot
 mkdir -p $VOL_DIR/u-boot
-cp $BINARIES_DIR/uboot.img $VOL_DIR/u-boot/uboot.img
-cp $BINARIES_DIR/boot.scr $VOL_DIR/u-boot/
-cp $BINARIES_DIR/vars.txt $VOL_DIR/u-boot/
 cp $BINARIES_DIR/trust.img $VOL_DIR/u-boot/
 cp $BINARIES_DIR/idbloader.img $VOL_DIR/u-boot/
+cp $BINARIES_DIR/uboot.img $VOL_DIR/u-boot/uboot.img
+#cp $BINARIES_DIR/boot.scr $VOL_DIR/u-boot/
+
+#boot
+mkdir -p $VOL_DIR/boot
+cp $BINARIES_DIR/vars.txt $VOL_DIR/boot/
+$MKIMAGE -C none -A arm -T script -d ${boardDir}/bootVolumio.cmd $BINARIES_DIR/bootVolumio.scr
+cp $BINARIES_DIR/bootVolumio.scr $VOL_DIR/boot/boot.scr
+cp ${boardDir}/varsVolumio.txt $VOL_DIR/boot/
 
 # Linux
-mkdir -p $VOL_DIR/boot
 cp $BINARIES_DIR/Image $VOL_DIR/boot/
 cp -a $BINARIES_DIR/rockchip $VOL_DIR/boot
-pushd $VOL_DIR/boot > /dev/null
-popd
+# pushd $VOL_DIR/boot > /dev/null
+# popd
 
 pushd $LINUX_DIR > /dev/null
 kver=`make kernelrelease`
