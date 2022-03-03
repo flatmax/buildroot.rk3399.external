8b2bb2ea4414e3c9ea4d20b4b9ad9957bbfdd30e
Matt Flax <flatmax@flatmax.org>
2022-03-04 10:41:38 +1100

    Remove BL32 from the rk3399 trust image. If it is resident, then we get a kernel panic.
    https://lore.kernel.org/linux-arm-kernel/20220301030031.4025282-1/

--- board/nanopi-neo4/mkRk3399Trust.bin.sh
+++ board/nanopi-neo4/mkRk3399Trust.bin.sh
@@ -10,12 +10,15 @@ SEC=1
 PATH=$2/bin/rk33/rk3399_bl31_v1.33.elf
 ADDR=0x00040000
 [BL32_OPTION]
-SEC=1
-PATH=$2/bin/rk33/rk3399_bl32_v1.22.bin
-ADDR=0x08400000
+SEC=0
 [BL33_OPTION]
 SEC=0
 [OUTPUT]
 PATH=$1/$3
 EOF
 $2/tools/trust_merger $1/RK3399TRUST.ini
+# The following give kernel panic when running the command stress
+# [BL32_OPTION]
+# SEC=1
+# PATH=$2/bin/rk33/rk3399_bl32_v1.22.bin
+# ADDR=0x08400000
