1e249decd337739e60610d2131026c2a8f7a1269
Matt Flax <flatmax@flatmax.com>
2022-06-20 06:16:45 +1000

    add jackd for testing.

---
+++ overlays/root/jackdStart.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+killall aplay
+killall jackd
+DIGIDEV=hw:CARD=deqxdigisoundca
+#amixer -D$DIGIDEV set 'Port B source' 'loopback'
+
+sleep 2
+#jackd -P 95 -d alsa -r 96000 &
+jackd -R -d alsa -r 96000 &
+sleep 2
+source ./jackdConnect.sh
